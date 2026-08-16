a81596e4ec68bff55ae4860378bd895c9974b509
jimmydhillon414-lgtm <jimmydhillon414@gmail.com>
2026-08-16 14:34:12 +0530

    Add files via upload

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,4 @@
+
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 import "./.next/dev/types/routes.d.ts";
@@ -5,3 +6,12 @@ import "./.next/dev/types/root-params.d.ts";
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
+
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+import "./.next/dev/types/routes.d.ts";
+import "./.next/dev/types/root-params.d.ts";
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
+
